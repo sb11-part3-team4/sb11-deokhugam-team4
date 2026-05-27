e90517da9cf0b29483f22c19c198530fba81cc63
Linpield <top09100620@gmail.com>
2026-05-27 11:16:46 +0900

    chore: cicd test 4

--- src/test/java/com/part3_team4/deokhoogam/DeokhoogamApplicationTests.java
+++ src/test/java/com/part3_team4/deokhoogam/DeokhoogamApplicationTests.java
@@ -2,8 +2,10 @@ package com.part3_team4.deokhoogam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DeokhoogamApplicationTests {
 
 	@Test
