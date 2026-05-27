0d20e7c01746d76fcd3ba981c061e822f755123e
kim gwanhee <rhksgml55@gmail.com>
2026-05-27 15:04:37 +0900

    feat: Review 관련 Repository 구현

---
+++ src/main/java/com/part3_team4/deokhoogam/domain/review/repository/ReviewLikeRepository.java
@@ -0,0 +1,11 @@
+package com.part3_team4.deokhoogam.domain.review.repository;
+
+import com.part3_team4.deokhoogam.domain.review.entity.ReviewLike;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewLikeRepository extends JpaRepository<ReviewLike, UUID> {
+
+    boolean existsByReviewIdAndUserId(UUID reviewId, UUID userId);
+
+}
