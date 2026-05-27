76f427c1a49311a843e52f223fd06be8e48786da
kim gwanhee <rhksgml55@gmail.com>
2026-05-28 00:59:45 +0900

    feat: 리뷰 등록 기본 구조 생성

--- src/main/java/com/part3_team4/deokhoogam/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/review/repository/ReviewRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID>{
 
+    boolean existsByUserIdAndBookId(UUID userId, UUID bookId);
+
 }
--- src/main/java/com/part3_team4/deokhoogam/domain/review/service/ReviewServiceImpl.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/review/service/ReviewServiceImpl.java
@@ -1,8 +1,22 @@
 package com.part3_team4.deokhoogam.domain.review.service;
 
+import com.part3_team4.deokhoogam.domain.book.repository.BookRepository;
+import com.part3_team4.deokhoogam.domain.review.dto.ReviewCreateRequest;
+import com.part3_team4.deokhoogam.domain.review.dto.ReviewResponse;
+import com.part3_team4.deokhoogam.domain.review.repository.ReviewRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReviewServiceImpl implements ReviewService{
+@RequiredArgsConstructor
+public class ReviewServiceImpl implements ReviewService {
 
+    private final ReviewRepository reviewRepository;
+    private final BookRepository bookRepository;
+
+    @Override
+    public ReviewResponse createReview(UUID userId, ReviewCreateRequest request) {
+        throw new UnsupportedOperationException("구현 전");
+    }
 }
