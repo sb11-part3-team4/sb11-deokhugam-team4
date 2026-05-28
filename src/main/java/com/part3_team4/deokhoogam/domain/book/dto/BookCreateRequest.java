bbac83258113285d006704665afa6dd3ae4ff18c
Ksinny <dosly2@naver.com>
2026-05-29 00:52:52 +0900

    refactor: [REFACTOR] 도서 등록 서비스 리팩터링 및 DTO 레코드 전환

--- src/main/java/com/part3_team4/deokhoogam/domain/book/dto/BookCreateRequest.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/book/dto/BookCreateRequest.java
@@ -1,39 +1,46 @@
 package com.part3_team4.deokhoogam.domain.book.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.part3_team4.deokhoogam.domain.book.entity.Book;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class BookCreateRequest {
+public record BookCreateRequest(
 
-  @NotBlank(message = "제목은 필수입니다")
-  @Length(max = 255, message = "제목은 255자 이하여야 합니다")
-  private String title;
+    @NotBlank(message = "제목은 필수입니다")
+    @Length(max = 255, message = "제목은 255자 이하여야 합니다")
+    String title,
 
-  @NotBlank(message = "저자는 필수입니다")
-  @Length(max = 100, message = "저자는 100자 이하여야 합니다")
-  private String author;
+    @NotBlank(message = "저자는 필수입니다")
+    @Length(max = 100, message = "저자는 100자 이하여야 합니다")
+    String author,
 
-  @NotBlank(message = "설명은 필수입니다")
-  private String description;
+    @NotBlank(message = "설명은 필수입니다")
+    String description,
 
-  @NotBlank(message = "출판사는 필수입니다")
-  @Length(max = 100, message = "출판사는 100자 이하여야 합니다")
-  private String publisher;
+    @NotBlank(message = "출판사는 필수입니다")
+    @Length(max = 100, message = "출판사는 100자 이하여야 합니다")
+    String publisher,
 
-  @NotNull(message = "출판일은 필수입니다")
-  @JsonFormat(pattern = "yyyy-MM-dd")
-  private LocalDate publishedDate;
+    @NotNull(message = "출판일은 필수입니다")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    LocalDate publishedDate,
 
-  private String isbn;
+    String isbn
+) {
+
+  public Book toEntity() {
+    return Book.builder()
+        .isbn(this.isbn)
+        .title(this.title)
+        .author(this.author)
+        .description(this.description)
+        .publisher(this.publisher)
+        .publishedDate(this.publishedDate)
+        .build();
+  }
 }
