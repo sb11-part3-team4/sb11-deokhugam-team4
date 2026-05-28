61d133a954227b5cacd80167cd2a58827036aa54
Ksinny <dosly2@naver.com>
2026-05-28 22:41:35 +0900

    test: [RED] ISBN 중복 검증 테스트 작성

--- src/test/java/com/part3_team4/deokhoogam/domain/book/BookRepositoryTest.java
+++ src/test/java/com/part3_team4/deokhoogam/domain/book/BookRepositoryTest.java
@@ -1,5 +1,49 @@
 package com.part3_team4.deokhoogam.domain.book;
 
-public class BookRepositoryTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.part3_team4.deokhoogam.domain.book.entity.Book;
+import com.part3_team4.deokhoogam.domain.book.repository.BookRepository;
+import com.part3_team4.deokhoogam.global.config.JpaAuditingConfig;
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@Import(JpaAuditingConfig.class)
+@ActiveProfiles("test")
+@DisplayName("BookRepository 테스트")
+class BookRepositoryTest {
+
+  @Autowired
+  private BookRepository bookRepository;
+
+  @Test
+  @DisplayName("ISBN 존재 여부를 확인한다")
+  void existsByIsbn_WhenBookExists_ReturnsTrue() {
+    // given
+    String targetIsbn = "1234567890123";
+
+    Book book = Book.builder()
+        .title("이펙티브 자바")
+        .author("조슈아 블로흐")
+        .description("자바 프로그래밍 가이드")
+        .publisher("인사이트")
+        .publishedDate(LocalDate.of(2026, 5, 28))
+        .isbn("1234567890123")
+        .build();
+    bookRepository.save(book);
+
+    // when
+    boolean exists = bookRepository.existsByIsbn(targetIsbn);
+    boolean notExists = bookRepository.existsByIsbn("0000000000000");
+
+    // then
+    assertThat(exists).isTrue();
+    assertThat(notExists).isFalse();
+  }
 }
