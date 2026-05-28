507a589c69844a69ee7a56b2c44765a2915fc242
Ksinny <dosly2@naver.com>
2026-05-29 00:09:03 +0900

    feat: [GREEN] 도서 등록 서비스 로직 구현

--- src/main/java/com/part3_team4/deokhoogam/domain/book/service/BookServiceImpl.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/book/service/BookServiceImpl.java
@@ -1,8 +1,36 @@
 package com.part3_team4.deokhoogam.domain.book.service;
 
+import com.part3_team4.deokhoogam.domain.book.dto.BookCreateRequest;
+import com.part3_team4.deokhoogam.domain.book.dto.BookDto;
+import com.part3_team4.deokhoogam.domain.book.entity.Book;
+import com.part3_team4.deokhoogam.domain.book.exception.IsbnAlreadyExistsException;
+import com.part3_team4.deokhoogam.domain.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class BookServiceImpl implements BookService{
+@RequiredArgsConstructor
+public class BookServiceImpl {
 
+  private final BookRepository bookRepository;
+
+  @Transactional
+  public BookDto create(BookCreateRequest request) {
+    if (bookRepository.existsByIsbn(request.getIsbn())) {
+      throw IsbnAlreadyExistsException.withIsbn(request.getIsbn());
+    }
+
+    Book book = Book.builder()
+        .isbn(request.getIsbn())
+        .title(request.getTitle())
+        .author(request.getAuthor())
+        .description(request.getDescription())
+        .publisher(request.getPublisher())
+        .publishedDate(request.getPublishedDate())
+        .build();
+
+    Book savedBook = bookRepository.save(book);
+    return BookDto.from(savedBook);
+  }
 }
