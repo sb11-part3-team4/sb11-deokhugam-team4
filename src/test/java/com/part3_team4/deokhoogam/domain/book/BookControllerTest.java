693838072a06010463cdfb6f715c4161ecaf46a4
Ksinny <dosly2@naver.com>
2026-05-29 01:44:14 +0900

    refactor: [REFACTOR] Controller 테스트 코드 내 중복 로직을 픽스처 메서드로 추출

--- src/test/java/com/part3_team4/deokhoogam/domain/book/BookControllerTest.java
+++ src/test/java/com/part3_team4/deokhoogam/domain/book/BookControllerTest.java
@@ -44,14 +44,7 @@ class BookControllerTest {
   @DisplayName("올바른 도서 정보로 생성 요청 시에 201 Created를 반환한다")
   void createBook_validRequest_returnsCreatedBook() throws Exception {
     // given
-    BookCreateRequest request = BookCreateRequest.builder()
-        .isbn("1234567890123")
-        .title("이펙티브 자바")
-        .author("조슈아 블로흐")
-        .description("자바 가이드")
-        .publisher("인사이트")
-        .publishedDate(LocalDate.of(2026, 5, 28))
-        .build();
+    BookCreateRequest request = createValidBookRequest();
 
     UUID mockId = UUID.randomUUID();
     BookDto mockResponse = BookDto.builder()
@@ -79,14 +72,7 @@ class BookControllerTest {
   @DisplayName("이미 존재하는 ISBN으로 도서를 등록 시에 409 Conflict를 반환한다")
   void createBook_alreadyIsbn_returnsConflict() throws Exception {
     // given
-    BookCreateRequest request = BookCreateRequest.builder()
-        .isbn("1234567890123")
-        .title("이펙티브 자바")
-        .author("조슈아 블로흐")
-        .description("자바 가이드")
-        .publisher("인사이트")
-        .publishedDate(LocalDate.of(2026, 5, 28))
-        .build();
+    BookCreateRequest request = createValidBookRequest();
 
     given(bookService.create(any(BookCreateRequest.class)))
         .willThrow(IsbnAlreadyExistsException.withIsbn(request.isbn()));
@@ -122,5 +108,17 @@ class BookControllerTest {
         .andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.code").value(ErrorCode.INVALID_INPUT_VALUE.getCode()))
         .andExpect(jsonPath("$.details.title").exists());
+  }
+
+  // 픽스처 메소드
+  private BookCreateRequest createValidBookRequest() {
+    return BookCreateRequest.builder()
+        .isbn("1234567890123")
+        .title("이펙티브 자바")
+        .author("조슈아 블로흐")
+        .description("자바 가이드")
+        .publisher("인사이트")
+        .publishedDate(LocalDate.of(2026, 5, 28))
+        .build();
   }
 }
