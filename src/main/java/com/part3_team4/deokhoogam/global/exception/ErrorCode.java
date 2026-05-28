5bd93bce3b6baf48b55c6eb4cf18747f824983e9
김지성 <shes3332@naver.com>
2026-05-28 16:32:50 +0900

    feat: ErrorCode에 사용자 예외 추가

--- src/main/java/com/part3_team4/deokhoogam/global/exception/ErrorCode.java
+++ src/main/java/com/part3_team4/deokhoogam/global/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
 
   // 1. 사용자 예외
   USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER-001", "존재하지 않는 사용자입니다."),
+  USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER-002", "이미 존재하는 사용자입니다."),
 
   // 2. 도서 예외
   BOOK_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK-001", "존재하지 않는 도서입니다."),
