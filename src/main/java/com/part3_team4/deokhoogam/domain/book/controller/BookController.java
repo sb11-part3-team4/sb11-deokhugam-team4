1ee95ff62f4b7cee9a08319af0aad335ae06ca4f
Ksinny <dosly2@naver.com>
2026-05-29 01:33:31 +0900

    feat: [GREEN] 도서 등록 컨트롤러 구현

--- src/main/java/com/part3_team4/deokhoogam/domain/book/controller/BookController.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/book/controller/BookController.java
@@ -1,9 +1,27 @@
 package com.part3_team4.deokhoogam.domain.book.controller;
 
-
+import com.part3_team4.deokhoogam.domain.book.dto.BookCreateRequest;
+import com.part3_team4.deokhoogam.domain.book.dto.BookDto;
+import com.part3_team4.deokhoogam.domain.book.service.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class BookController {
 
+  private final BookService bookService;
+
+  @PostMapping
+  public ResponseEntity<BookDto> createBook(@Valid @RequestBody BookCreateRequest request) {
+    BookDto response = bookService.create(request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
 }
