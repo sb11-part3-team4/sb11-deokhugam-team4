79a3c1176c018fe23f4a4f749fe07d6c1136b190
Ksinny <dosly2@naver.com>
2026-05-29 11:44:11 +0900

    refactor: BookService 내 ISBN 동시성 데이터 예외 처리 로직 추가

--- src/main/java/com/part3_team4/deokhoogam/domain/book/service/BookServiceImpl.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/book/service/BookServiceImpl.java
@@ -6,6 +6,7 @@ import com.part3_team4.deokhoogam.domain.book.entity.Book;
 import com.part3_team4.deokhoogam.domain.book.exception.IsbnAlreadyExistsException;
 import com.part3_team4.deokhoogam.domain.book.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,13 +16,19 @@ public class BookServiceImpl implements BookService {
 
   private final BookRepository bookRepository;
 
+  @Override
   @Transactional
   public BookDto create(BookCreateRequest request) {
     if (bookRepository.existsByIsbn(request.isbn())) {
       throw IsbnAlreadyExistsException.withIsbn(request.isbn());
     }
 
-    Book book = request.toEntity();
-    return BookDto.from(bookRepository.save(book));
+    // 동시성 문제 방지
+    try {
+      Book book = request.toEntity();
+      return BookDto.from(bookRepository.save(book));
+    } catch (DataIntegrityViolationException e) {
+      throw IsbnAlreadyExistsException.withIsbn(request.isbn());
+    }
   }
 }
