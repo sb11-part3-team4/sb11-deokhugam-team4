99f7112787214a3fb1df9b2b26c5e55eeb8d2738
Ksinny <dosly2@naver.com>
2026-05-29 20:16:46 +0900

    feat: [GREEN] 도서 수정 서비스 로직 구현

--- src/main/java/com/part3_team4/deokhoogam/domain/book/service/BookServiceImpl.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/book/service/BookServiceImpl.java
@@ -2,9 +2,12 @@ package com.part3_team4.deokhoogam.domain.book.service;
 
 import com.part3_team4.deokhoogam.domain.book.dto.BookCreateRequest;
 import com.part3_team4.deokhoogam.domain.book.dto.BookDto;
+import com.part3_team4.deokhoogam.domain.book.dto.BookUpdateRequest;
 import com.part3_team4.deokhoogam.domain.book.entity.Book;
+import com.part3_team4.deokhoogam.domain.book.exception.BookNotFoundException;
 import com.part3_team4.deokhoogam.domain.book.exception.IsbnAlreadyExistsException;
 import com.part3_team4.deokhoogam.domain.book.repository.BookRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -30,5 +33,23 @@ public class BookServiceImpl implements BookService {
     } catch (DataIntegrityViolationException e) {
       throw IsbnAlreadyExistsException.withIsbn(request.isbn());
     }
+  }
+
+  @Override
+  @Transactional
+  public BookDto update(UUID id, BookUpdateRequest request) {
+    Book book = bookRepository.findById(id)
+        .orElseThrow(() -> BookNotFoundException.withId(id));
+
+    book.updateBookInfo(
+        request.getTitle(),
+        request.getAuthor(),
+        request.getDescription(),
+        request.getPublisher(),
+        request.getPublishedDate(),
+        book.getThumbnailUrl() // TODO: 추후 메서드 분리 및 관련 로직 추가 예정
+    );
+
+    return BookDto.from(book);
   }
 }
