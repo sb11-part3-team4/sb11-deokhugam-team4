ea4e1a6136e5d62bdbe22b4acb6d67838e0e1245
kim gwanhee <rhksgml55@gmail.com>
2026-05-29 15:55:54 +0900

    refactor: InvalidReviewException 파라미터 줄바꿈 정리

--- src/main/java/com/part3_team4/deokhoogam/domain/review/exception/InvalidReviewException.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/review/exception/InvalidReviewException.java
@@ -1,7 +1,26 @@
 package com.part3_team4.deokhoogam.domain.review.exception;
 
-public class InvalidReviewException extends RuntimeException {
-  public InvalidReviewException(String message) {
-    super(message);
-  }
+import com.part3_team4.deokhoogam.global.exception.ErrorCode;
+import com.part3_team4.deokhoogam.global.exception.ErrorKey;
+
+public class InvalidReviewException extends ReviewException {
+
+    private InvalidReviewException() {
+        super(ErrorCode.REVIEW_INVALID_INPUT);
+    }
+
+    public static InvalidReviewException withField(ErrorKey field, String reason) {
+        InvalidReviewException exception = new InvalidReviewException();
+        exception.addDetail(ErrorKey.FIELD, field.getValue());
+        exception.addDetail(ErrorKey.REASON, reason);
+        return exception;
+    }
+
+    public static InvalidReviewException withFieldAndValue(ErrorKey field, Object value, String reason) {
+        InvalidReviewException exception = new InvalidReviewException();
+        exception.addDetail(ErrorKey.FIELD, field.getValue());
+        exception.addDetail(ErrorKey.VALUE, value);
+        exception.addDetail(ErrorKey.REASON, reason);
+        return exception;
+    }
 }
