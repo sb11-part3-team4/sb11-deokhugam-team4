8cfc256dc5820e44c3c32b0330ab31d918561c1b
김지성 <shes3332@naver.com>
2026-06-02 11:22:45 +0900

    fix: JPA 매핑 에러 방지, DeletedUser 시간 필드 추가 및 미사용 코드 정리

--- src/main/java/com/part3_team4/deokhoogam/domain/user/mapper/UserMapper.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/user/mapper/UserMapper.java
@@ -1,9 +1,7 @@
 package com.part3_team4.deokhoogam.domain.user.mapper;
 
 import com.part3_team4.deokhoogam.domain.user.dto.UserDto;
-import com.part3_team4.deokhoogam.domain.user.dto.UserProfileImageDto;
 import com.part3_team4.deokhoogam.domain.user.entity.User;
-import com.part3_team4.deokhoogam.domain.user.entity.UserProfileImage;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,29 +11,11 @@ public class UserMapper {
       return null;
     }
 
-    UserProfileImageDto profileDto = null;
-    if (user.getProfileImage() != null) {
-      profileDto = toProfileImageDto(user.getProfileImage());
-    }
-
     return new UserDto(
         user.getId(),
         user.getEmail(),
         user.getName(),
-        profileDto
-    );
-  }
-
-  public UserProfileImageDto toProfileImageDto(UserProfileImage profileImage) {
-    if (profileImage == null) {
-      return null;
-    }
-
-    return new UserProfileImageDto(
-        profileImage.getId(),
-        profileImage.getFileName(),
-        profileImage.getSize(),
-        profileImage.getContentType()
+        null
     );
   }
 }
