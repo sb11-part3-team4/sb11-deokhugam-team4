54ce436c7a55b46daceb000087c0274d5cbf529a
Linpield <top09100620@gmail.com>
2026-06-02 13:44:52 +0900

    feat: [red] 도서 목록 페이지네이션 적용 가져오기 리포지토리 테스트 케이스 작성

--- src/test/java/com/part3_team4/deokhoogam/domain/book/BookRepositoryTest.java
+++ src/test/java/com/part3_team4/deokhoogam/domain/book/BookRepositoryTest.java
@@ -2,15 +2,26 @@ package com.part3_team4.deokhoogam.domain.book;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.part3_team4.deokhoogam.domain.book.Factory.BookFixtureFactory;
+import com.part3_team4.deokhoogam.domain.book.dto.BookCursor;
+import com.part3_team4.deokhoogam.domain.book.dto.BookGetListRequest;
 import com.part3_team4.deokhoogam.domain.book.entity.Book;
+import com.part3_team4.deokhoogam.domain.book.entity.Direction;
+import com.part3_team4.deokhoogam.domain.book.entity.SortType;
 import com.part3_team4.deokhoogam.domain.book.repository.BookRepository;
 import com.part3_team4.deokhoogam.global.config.JpaAuditingConfig;
+import com.part3_team4.deokhoogam.global.config.QuerydslConfig;
+import com.part3_team4.deokhoogam.global.util.CursorUtils;
 import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Slice;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
@@ -50,4 +61,95 @@ class BookRepositoryTest {
         .isbn(isbn)
         .build();
   }
+
+
+  @DataJpaTest
+  @Import(QuerydslConfig.class)
+  @DisplayName("BookRepository 커서 페이지네이션 에서")
+  @Nested
+  class TestCursorPagination {
+
+
+    @BeforeEach
+    void setUp() {
+
+      List<Book> savedBooks = BookFixtureFactory.createBookList();
+      bookRepository.saveAll(savedBooks);
+
+    }
+
+
+    @Test
+    @DisplayName("커서가 없으면 최신 데이터부터 limit만큼 가져온다")
+    void getBooks_firstPage() {
+      // given
+      BookGetListRequest request = new BookGetListRequest("", SortType.TITLE, Direction.DESC,
+          null, null, 2);
+      BookCursor cursor = null; // 첫 요청
+
+      List<Book> savedBooks = BookFixtureFactory.createBookList();
+
+      // when
+      Slice<Book> result = bookRepository.getBooks(cursor, request);
+
+      // then
+      assertThat(result.getContent()).hasSize(2); // limit 개수만큼 가져왔는가?
+      assertThat(result.hasNext()).isTrue(); // 다음 페이지가 있는가?
+
+      // 정렬이 최신순으로 잘 되었는가? (미리 저장해둔 최신순 리스트의 0, 1번 인덱스와 비교)
+      assertThat(result.getContent().get(0).getTitle()).isEqualTo(savedBooks.get(0).getTitle());
+      assertThat(result.getContent().get(1).getTitle()).isEqualTo(savedBooks.get(1).getTitle());
+    }
+
+    @Test
+    @DisplayName("커서 값 이후의 데이터를 정상적으로 가져오고, 마지막 페이지일시 hasnext = fasle를 반환한다")
+    void getBooks_nextPage() {
+
+      List<Book> savedBooks = BookFixtureFactory.createBookList();
+
+      Book lastBookOfFirstPage = savedBooks.get(1);
+
+      BookCursor testCursor = new BookCursor( //커서.
+          lastBookOfFirstPage.getTitle(),
+          lastBookOfFirstPage.getId(),
+          lastBookOfFirstPage.getCreatedAt()
+      );
+
+      String cursor = CursorUtils.encodeCursor(testCursor); //인코딩
+
+      BookGetListRequest request = new BookGetListRequest("", SortType.TITLE, Direction.DESC,
+          cursor, lastBookOfFirstPage.getCreatedAt(), 2);
+
+      // when
+      Slice<Book> result = bookRepository.getBooks(testCursor, request);
+
+      // then
+      assertThat(result.getContent()).hasSize(2); // 총 네개. 두개를 가져오고 두개를 추가로 가져온 상황
+      assertThat(result.hasNext()).isFalse(); //
+
+
+      assertThat(result.getContent().get(0).getId()).isEqualTo(savedBooks.get(2).getId());
+      assertThat(result.getContent().get(1).getId()).isEqualTo(savedBooks.get(3).getId());
+    }
+
+
+  }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
