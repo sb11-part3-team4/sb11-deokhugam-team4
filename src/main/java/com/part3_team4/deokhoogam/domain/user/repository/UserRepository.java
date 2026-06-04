ac00ae8ef9ef296e6092b07a78d2be107389ea58
김지성 <shes3332@naver.com>
2026-06-04 14:15:40 +0900

    fix: 코드래빗 피드백 반영

--- src/main/java/com/part3_team4/deokhoogam/domain/user/repository/UserRepository.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/user/repository/UserRepository.java
@@ -8,12 +8,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByName(String name);
   boolean existsByEmail(String email);
   Optional<User> findByEmail(String email);
 
+  @Transactional
   @Modifying
   @Query(value = "DELETE FROM \"user\" WHERE id = :id", nativeQuery = true)
   void hardDeleteById(@Param("id") UUID id);
