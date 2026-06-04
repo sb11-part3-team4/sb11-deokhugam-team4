71428e742a2ce64f7b24da3cf0f1dae52f4e7b07
Ksinny <dosly2@naver.com>
2026-06-04 18:57:27 +0900

    feat: [GREEN] OCR 이미지 텍스트 추출 클라이언트 구현 및 api 키 설정 추가

--- src/main/java/com/part3_team4/deokhoogam/domain/book/infrastructure/ocr/OcrSpaceApiClient.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/book/infrastructure/ocr/OcrSpaceApiClient.java
@@ -1,6 +1,12 @@
 package com.part3_team4.deokhoogam.domain.book.infrastructure.ocr;
 
+import com.part3_team4.deokhoogam.domain.book.exception.OcrProcessingException;
+import com.part3_team4.deokhoogam.domain.book.infrastructure.ocr.dto.OcrSpaceDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,13 +14,36 @@ import org.springframework.web.multipart.MultipartFile;
 public class OcrSpaceApiClient {
 
   private final RestClient restClient;
+  private final String apiKey;
 
-  public OcrSpaceApiClient(RestClient.Builder builder) {
-    this.restClient = builder.build();
+  public OcrSpaceApiClient(
+      RestClient.Builder builder,
+      @Value("${ocr.space.api-key}") String apiKey) {
+
+    this.apiKey = apiKey;
+    this.restClient = builder
+        .baseUrl("https://api.ocr.space")
+        .build();
   }
 
-  // 텍스트 추출
   public String extractTextFromImage(MultipartFile file) {
-    return null;
+    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+    body.add("file", file.getResource());
+    body.add("language", "kor");
+
+    OcrSpaceDto response = restClient.post()
+        .uri("/parse/image")
+        .header("apikey", apiKey)
+        .contentType(MediaType.MULTIPART_FORM_DATA)
+        .body(body)
+        .retrieve()
+        .body(OcrSpaceDto.class);
+
+    if (response == null || response.isErroredOnProcessing() || response.getParsedResults()
+        .isEmpty()) {
+      throw new OcrProcessingException("OCR 이미지 처리 중 오류가 발생했습니다.");
+    }
+    
+    return response.getParsedResults().get(0).getParsedText();
   }
 }
