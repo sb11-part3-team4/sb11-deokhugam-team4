881594a9c8f6899456758c7613cba3cba812da37
Linpield_62 <80905921+Jeongyun-Choi62@users.noreply.github.com>
2026-06-07 01:04:06 +0900

    [FIX] isbn으로 도서 정보 조회 수정. (#97)
    * fix: 응답 추가 및 dto 변환, locadate 형식 변환, 이미지 파일 인코딩
    
    * fix: code rabbit 1차 리뷰 반영

--- src/main/java/com/part3_team4/deokhoogam/domain/book/dto/NaverBookDto.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/book/dto/NaverBookDto.java
@@ -1,12 +1,18 @@
 package com.part3_team4.deokhoogam.domain.book.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.part3_team4.deokhoogam.global.util.DateDeserializer;
+import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -17,11 +23,12 @@ public class NaverBookDto {
   private String description;
   private String publisher;
 
-  @JsonProperty("pubdate")
-  private String publishedDate;
-
+  @JsonAlias("pubdate")
+  @JsonDeserialize(using = DateDeserializer.class)
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+  private LocalDate publishedDate;
   private String isbn;
 
-  @JsonProperty("image")
+  @JsonAlias("image")
   private String thumbnailImage;
 }
