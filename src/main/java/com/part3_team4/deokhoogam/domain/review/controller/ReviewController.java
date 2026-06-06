d1639ef6a414f3ba4b9c5734bbc84b8c8f576a28
Linpield <top09100620@gmail.com>
2026-06-07 00:56:28 +0900

    chore: review controller DeleteMapping 라이브러리 임포트 안된것 수정

--- src/main/java/com/part3_team4/deokhoogam/domain/review/controller/ReviewController.java
+++ src/main/java/com/part3_team4/deokhoogam/domain/review/controller/ReviewController.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
