e42b133e55631b8d08cafc9686d6c8a4e865a7f9
Linpield <top09100620@gmail.com>
2026-06-09 11:15:43 +0900

    fix: 로그 s3 저장 prod 프로필 한정

--- src/main/java/com/part3_team4/deokhoogam/global/scheduler/LogUploadScheduler.java
+++ src/main/java/com/part3_team4/deokhoogam/global/scheduler/LogUploadScheduler.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -13,6 +14,7 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 @Slf4j
 @Component
+@Profile("prod")
 @RequiredArgsConstructor
 public class LogUploadScheduler {
 
