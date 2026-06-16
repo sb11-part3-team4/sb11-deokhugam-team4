9fe6ebf6e70f3b3838daff4ed3f8990cf16913b8
Ksinny <102038572+Ksinny@users.noreply.github.com>
2026-06-16 14:07:16 +0900

    [REFACTOR] 도서 도메인 코드 가독성 개선 리팩토링 (#204)
    * chore: BookController 메서드의 Swagger 관련 애노테이션을 BookApi 인터페이스로 이동
    
    * chore: Book 도메인 테스트에 전역적으로 @Nested 적용

--- src/test/java/com/part3_team4/deokhoogam/domain/book/BookControllerTest.java
+++ src/test/java/com/part3_team4/deokhoogam/domain/book/BookControllerTest.java
@@ -74,218 +74,233 @@ class BookControllerTest {
   @MockitoBean
   private BookRankingService bookRankingService;
 
-  @Test
-  @DisplayName("올바른 도서 정보로 생성 요청 시에 201 Created를 반환한다")
-  void createBook_validRequest_returnsCreatedBook() throws Exception {
-    // given
-    BookCreateRequest request = BookFixtures.validBookCreateRequest();
-    UUID mockId = UUID.randomUUID();
+  @Nested
+  @DisplayName("도서 등록 API에서")
+  class TestCreateBook {
 
-    BookDto mockResponse = BookDto.builder()
-        .id(mockId)
-        .isbn(request.isbn())
-        .title(request.title())
-        .author(request.author())
-        .build();
+    @Test
+    @DisplayName("올바른 도서 정보로 생성 요청 시에 201 Created를 반환한다")
+    void createBook_validRequest_returnsCreatedBook() throws Exception {
+      // given
+      BookCreateRequest request = BookFixtures.validBookCreateRequest();
+      UUID mockId = UUID.randomUUID();
 
-    given(bookService.create(any(BookCreateRequest.class), isNull()))
-        .willReturn(mockResponse);
+      BookDto mockResponse = BookDto.builder()
+          .id(mockId)
+          .isbn(request.isbn())
+          .title(request.title())
+          .author(request.author())
+          .build();
 
-    // when & then
-    mockMvc.perform(createBookRequest(request))
-        .andDo(print())
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.id").value(mockId.toString()))
-        .andExpect(jsonPath("$.isbn").value(request.isbn()))
-        .andExpect(jsonPath("$.title").value(request.title()));
+      given(bookService.create(any(BookCreateRequest.class), isNull()))
+          .willReturn(mockResponse);
+
+      // when & then
+      mockMvc.perform(createBookRequest(request))
+          .andDo(print())
+          .andExpect(status().isCreated())
+          .andExpect(jsonPath("$.id").value(mockId.toString()))
+          .andExpect(jsonPath("$.isbn").value(request.isbn()))
+          .andExpect(jsonPath("$.title").value(request.title()));
+    }
+
+    @Test
+    @DisplayName("썸네일 파일과 함께 도서 생성 요청 시 201 Created를 반환한다")
+    void createBook_withThumbnail_returnsCreatedBook() throws Exception {
+      // given
+      BookCreateRequest request = BookFixtures.validBookCreateRequest();
+
+      UUID mockId = UUID.randomUUID();
+
+      BookDto mockResponse = BookDto.builder()
+          .id(mockId)
+          .title(request.title())
+          .isbn(request.isbn())
+          .author(request.author())
+          .build();
+
+      given(bookService.create(any(BookCreateRequest.class), any()))
+          .willReturn(mockResponse);
+
+      // when & then
+      mockMvc.perform(createBookRequest(request, createThumbnailFile()))
+          .andDo(print())
+          .andExpect(status().isCreated())
+          .andExpect(jsonPath("$.id").value(mockId.toString()))
+          .andExpect(jsonPath("$.title").value(request.title()));
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 ISBN으로 도서를 등록 시에 409 Conflict를 반환한다")
+    void createBook_alreadyIsbn_returnsConflict() throws Exception {
+      // given
+      BookCreateRequest request = BookFixtures.validBookCreateRequest();
+
+      given(bookService.create(any(BookCreateRequest.class), isNull()))
+          .willThrow(IsbnAlreadyExistsException.withIsbn(request.isbn()));
+
+      // when & then
+      mockMvc.perform(createBookRequest(request))
+          .andDo(print())
+          .andExpect(status().isConflict())
+          .andExpect(jsonPath("$.code").value(ErrorCode.ISBN_ALREADY_EXISTS.getCode()))
+          .andExpect(jsonPath("$.message").value(ErrorCode.ISBN_ALREADY_EXISTS.getMessage()));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 정보로 생성 요청 시에 400 Bad Request를 반환한다")
+    void createBook_invalidRequest_returnsBadRequest() throws Exception {
+      // given
+      BookCreateRequest invalidRequest = BookFixtures.validBookCreateRequest().toBuilder()
+          .title("")
+          .build();
+
+      // when & then
+      mockMvc.perform(createBookRequest(invalidRequest))
+          .andDo(print())
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.code").value(ErrorCode.INVALID_INPUT_VALUE.getCode()))
+          .andExpect(jsonPath("$.details.title").exists());
+    }
   }
 
-  @Test
-  @DisplayName("썸네일 파일과 함께 도서 생성 요청 시 201 Created를 반환한다")
-  void createBook_withThumbnail_returnsCreatedBook() throws Exception {
-    // given
-    BookCreateRequest request = BookFixtures.validBookCreateRequest();
+  @Nested
+  @DisplayName("도서 수정 API에서")
+  class TestUpdateBook {
 
-    UUID mockId = UUID.randomUUID();
+    @Test
+    @DisplayName("올바른 도서 정보로 수정 요청 시에 200 OK를 반환한다")
+    void updateBook_validRequest_returnsOk() throws Exception {
+      // given
+      UUID targetId = UUID.randomUUID();
+      BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
 
-    BookDto mockResponse = BookDto.builder()
-        .id(mockId)
-        .title(request.title())
-        .isbn(request.isbn())
-        .author(request.author())
-        .build();
+      BookDto mockResponse = BookDto.builder()
+          .id(targetId)
+          .title(request.title())
+          .author(request.author())
+          .description(request.description())
+          .publisher(request.publisher())
+          .publishedDate(request.publishedDate())
+          .build();
 
-    given(bookService.create(any(BookCreateRequest.class), any()))
-        .willReturn(mockResponse);
+      given(bookService.update(any(UUID.class), any(BookUpdateRequest.class), isNull()))
+          .willReturn(mockResponse);
 
-    // when & then
-    mockMvc.perform(createBookRequest(request, createThumbnailFile()))
-        .andDo(print())
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.id").value(mockId.toString()))
-        .andExpect(jsonPath("$.title").value(request.title()));
+      // when & then
+      mockMvc.perform(updateBookRequest(targetId, request))
+          .andDo(print())
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.title").value(request.title()));
+    }
+
+    @Test
+    @DisplayName("썸네일 파일과 함께 도서 수정 요청 시 200 OK를 반환한다")
+    void updateBook_withThumbnail_returnsOk() throws Exception {
+      // given
+      UUID targetId = UUID.randomUUID();
+      BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
+      MockMultipartFile thumbnail = createThumbnailFile();
+
+      BookDto mockResponse = BookDto.builder()
+          .id(targetId)
+          .title(request.title())
+          .build();
+
+      given(bookService.update(eq(targetId), any(BookUpdateRequest.class), any()))
+          .willReturn(mockResponse);
+
+      // when & then
+      mockMvc.perform(updateBookRequest(targetId, request, thumbnail))
+          .andDo(print())
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.title").value(request.title()));
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 정보로 수정 요청 시에 400 Bad Request를 반환한다")
+    void updateBook_invalidRequest_returnsBadRequest() throws Exception {
+      // given
+      UUID targetId = UUID.randomUUID();
+      BookUpdateRequest invalidRequest = BookFixtures.validBookUpdateRequest().toBuilder()
+          .title("")
+          .build();
+
+      // when & then
+      mockMvc.perform(updateBookRequest(targetId, invalidRequest))
+          .andDo(print())
+          .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 도서 ID로 수정 요청 시에 404 Not Found를 반환한다")
+    void updateBook_nonExistentId_returnsNotFound() throws Exception {
+      // given
+      UUID nonExistentId = UUID.randomUUID();
+      BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
+
+      // 수정 후
+      given(bookService.update(any(UUID.class), any(BookUpdateRequest.class), isNull()))
+          .willThrow(BookNotFoundException.withId(nonExistentId));
+
+      // when & then
+      mockMvc.perform(updateBookRequest(nonExistentId, request))
+          .andDo(print())
+          .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("도서 수정 중 ISBN 중복 예외 발생 시에 409 Conflict를 반환한다")
+    void updateBook_isbnConflict_returnsConflict() throws Exception {
+      // given
+      UUID targetId = UUID.randomUUID();
+
+      BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
+
+      given(bookService.update(any(UUID.class), any(BookUpdateRequest.class), isNull()))
+          .willThrow(IsbnAlreadyExistsException.withIsbn("1234567890123"));
+
+      // when & then
+      mockMvc.perform(updateBookRequest(targetId, request))
+          .andDo(print())
+          .andExpect(status().isConflict());
+    }
   }
 
-  @Test
-  @DisplayName("이미 존재하는 ISBN으로 도서를 등록 시에 409 Conflict를 반환한다")
-  void createBook_alreadyIsbn_returnsConflict() throws Exception {
-    // given
-    BookCreateRequest request = BookFixtures.validBookCreateRequest();
+  @Nested
+  @DisplayName("도서 OCR 인식 API에서")
+  class TestOCRBook {
 
-    given(bookService.create(any(BookCreateRequest.class), isNull()))
-        .willThrow(IsbnAlreadyExistsException.withIsbn(request.isbn()));
+    @Test
+    @DisplayName("이미지를 업로드하면 ISBN을 문자열로 반환한다")
+    void extractIsbn_validImage_returnsOk() throws Exception {
+      // given
+      MockMultipartFile image = createOcrImageFile();
 
-    // when & then
-    mockMvc.perform(createBookRequest(request))
-        .andDo(print())
-        .andExpect(status().isConflict())
-        .andExpect(jsonPath("$.code").value(ErrorCode.ISBN_ALREADY_EXISTS.getCode()))
-        .andExpect(jsonPath("$.message").value(ErrorCode.ISBN_ALREADY_EXISTS.getMessage()));
-  }
+      given(ocrService.extractIsbnFromImage(any())).willReturn("9788965402602");
 
-  @Test
-  @DisplayName("유효하지 않은 정보로 생성 요청 시에 400 Bad Request를 반환한다")
-  void createBook_invalidRequest_returnsBadRequest() throws Exception {
-    // given
-    BookCreateRequest invalidRequest = BookFixtures.validBookCreateRequest().toBuilder()
-        .title("")
-        .build();
+      // when & then
+      mockMvc.perform(extractIsbnRequest(image))
+          .andDo(print())
+          .andExpect(status().isOk())
+          .andExpect(content().string("9788965402602"));
+    }
 
-    // when & then
-    mockMvc.perform(createBookRequest(invalidRequest))
-        .andDo(print())
-        .andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.code").value(ErrorCode.INVALID_INPUT_VALUE.getCode()))
-        .andExpect(jsonPath("$.details.title").exists());
-  }
+    @Test
+    @DisplayName("OCR 인식 실패 시 400 Bad Request를 반환한다")
+    void extractIsbn_ocrFails_returnsBadRequest() throws Exception {
+      // given
+      MockMultipartFile image = createOcrImageFile();
 
-  @Test
-  @DisplayName("올바른 도서 정보로 수정 요청 시에 200 OK를 반환한다")
-  void updateBook_validRequest_returnsOk() throws Exception {
-    // given
-    UUID targetId = UUID.randomUUID();
-    BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
+      given(ocrService.extractIsbnFromImage(any()))
+          .willThrow(OcrProcessingException.from(ErrorCode.OCR_PROCESSING_FAILED));
 
-    BookDto mockResponse = BookDto.builder()
-        .id(targetId)
-        .title(request.title())
-        .author(request.author())
-        .description(request.description())
-        .publisher(request.publisher())
-        .publishedDate(request.publishedDate())
-        .build();
-
-    given(bookService.update(any(UUID.class), any(BookUpdateRequest.class), isNull()))
-        .willReturn(mockResponse);
-
-    // when & then
-    mockMvc.perform(updateBookRequest(targetId, request))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.title").value(request.title()));
-  }
-
-  @Test
-  @DisplayName("썸네일 파일과 함께 도서 수정 요청 시 200 OK를 반환한다")
-  void updateBook_withThumbnail_returnsOk() throws Exception {
-    // given
-    UUID targetId = UUID.randomUUID();
-    BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
-    MockMultipartFile thumbnail = createThumbnailFile();
-
-    BookDto mockResponse = BookDto.builder()
-        .id(targetId)
-        .title(request.title())
-        .build();
-
-    given(bookService.update(eq(targetId), any(BookUpdateRequest.class), any()))
-        .willReturn(mockResponse);
-
-    // when & then
-    mockMvc.perform(updateBookRequest(targetId, request, thumbnail))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.title").value(request.title()));
-  }
-
-  @Test
-  @DisplayName("유효하지 않은 정보로 수정 요청 시에 400 Bad Request를 반환한다")
-  void updateBook_invalidRequest_returnsBadRequest() throws Exception {
-    // given
-    UUID targetId = UUID.randomUUID();
-    BookUpdateRequest invalidRequest = BookFixtures.validBookUpdateRequest().toBuilder()
-        .title("")
-        .build();
-
-    // when & then
-    mockMvc.perform(updateBookRequest(targetId, invalidRequest))
-        .andDo(print())
-        .andExpect(status().isBadRequest());
-  }
-
-  @Test
-  @DisplayName("존재하지 않는 도서 ID로 수정 요청 시에 404 Not Found를 반환한다")
-  void updateBook_nonExistentId_returnsNotFound() throws Exception {
-    // given
-    UUID nonExistentId = UUID.randomUUID();
-    BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
-
-    // 수정 후
-    given(bookService.update(any(UUID.class), any(BookUpdateRequest.class), isNull()))
-        .willThrow(BookNotFoundException.withId(nonExistentId));
-
-    // when & then
-    mockMvc.perform(updateBookRequest(nonExistentId, request))
-        .andDo(print())
-        .andExpect(status().isNotFound());
-  }
-
-  @Test
-  @DisplayName("도서 수정 중 ISBN 중복 예외 발생 시에 409 Conflict를 반환한다")
-  void updateBook_isbnConflict_returnsConflict() throws Exception {
-    // given
-    UUID targetId = UUID.randomUUID();
-
-    BookUpdateRequest request = BookFixtures.validBookUpdateRequest();
-
-    given(bookService.update(any(UUID.class), any(BookUpdateRequest.class), isNull()))
-        .willThrow(IsbnAlreadyExistsException.withIsbn("1234567890123"));
-
-    // when & then
-    mockMvc.perform(updateBookRequest(targetId, request))
-        .andDo(print())
-        .andExpect(status().isConflict());
-  }
-
-  @Test
-  @DisplayName("이미지를 업로드하면 ISBN을 문자열로 반환한다")
-  void extractIsbn_validImage_returnsOk() throws Exception {
-    // given
-    MockMultipartFile image = createOcrImageFile();
-
-    given(ocrService.extractIsbnFromImage(any())).willReturn("9788965402602");
-
-    // when & then
-    mockMvc.perform(extractIsbnRequest(image))
-        .andDo(print())
-        .andExpect(status().isOk())
-        .andExpect(content().string("9788965402602"));
-  }
-
-  @Test
-  @DisplayName("OCR 인식 실패 시 400 Bad Request를 반환한다")
-  void extractIsbn_ocrFails_returnsBadRequest() throws Exception {
-    // given
-    MockMultipartFile image = createOcrImageFile();
-
-    given(ocrService.extractIsbnFromImage(any()))
-        .willThrow(OcrProcessingException.from(ErrorCode.OCR_PROCESSING_FAILED));
-
-    // when & then
-    mockMvc.perform(extractIsbnRequest(image))
-        .andDo(print())
-        .andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.code").value(ErrorCode.OCR_PROCESSING_FAILED.getCode()));
+      // when & then
+      mockMvc.perform(extractIsbnRequest(image))
+          .andDo(print())
+          .andExpect(status().isBadRequest())
+          .andExpect(jsonPath("$.code").value(ErrorCode.OCR_PROCESSING_FAILED.getCode()));
+    }
   }
 
   // ============ 헬퍼 메서드 =============
@@ -500,7 +515,6 @@ class BookControllerTest {
 
         PageResponse<BookDto> response = new PageResponse<>(
 
-
             books,
             "Cursor",
             "After",
@@ -551,7 +565,7 @@ class BookControllerTest {
         result.andExpect(status().isBadRequest())
             .andExpect(jsonPath("$.code").value(ErrorCode.INVALID_INPUT_VALUE.getCode()))
             .andExpect(jsonPath("$.message").exists())
-            .andExpect(jsonPath("$.details.limit").exists());;
+            .andExpect(jsonPath("$.details.limit").exists());
 
 
       }
@@ -623,9 +637,6 @@ class BookControllerTest {
           .andExpect(status().isNotFound());
 
     }
-
-
-
 
 
   }
